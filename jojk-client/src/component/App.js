42aa820680925dfa642705567ea840f5c17bd61f
oskaremilsson <oskaremilsson@gmail.com>
2017-09-27 22:45:53 +0200

    Adding location

--- jojk-client/src/component/App.js
+++ jojk-client/src/component/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Sidebar from 'react-sidebar';
+import axios from 'axios';
+import config from './../../config.json';
 
 import MyNowPlaying from './MyNowPlaying';
 import Menu from './Menu';
-import Profile from './Profile';
+import Billboard from './Billboard';
 
 import './../styles/App.css';
 
@@ -17,7 +19,8 @@ class App extends Component {
         this.state = {
             mql: mql,
             docked: props.docked,
-            open: props.open
+            open: props.open,
+            location: undefined
         }
 
         this.sidebarStyle = {
@@ -29,6 +32,11 @@ class App extends Component {
             }
         };
 
+        this.mapsApi = axios.create({
+            baseURL: config.maps.baseURL,
+            timeout: 1000
+        });
+
         this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
         this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
         this.closeSidebar = this.closeSidebar.bind(this);
@@ -36,18 +44,51 @@ class App extends Component {
     componentWillMount() {
         mql.addListener(this.mediaQueryChanged);
         this.setState({mql: mql, sidebarDocked: mql.matches});
+        this.getLocation();
     }
+
     componentWillUnmount() {
         this.setState({sidebarDocked: this.state.mql.matches});
     }
+
     mediaQueryChanged() {
         this.setState({sidebarDocked: this.state.mql.matches});
     }
+
     onSetSidebarOpen(open) {
         this.setState({sidebarOpen: open});
     }
+
     closeSidebar() {
         this.setState({sidebarOpen: false});
+    }
+    
+    getLocation() {
+        var _this = this;
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function(pos) {
+                _this.mapsApi.get('json?latlng='+pos.coords.latitude+','+pos.coords.longitude+'&location_type=APPROXIMATE&result_type=locality&key='+config.firebase.apiKey)
+                .then(res => {
+                    var location = res.data.results[0].address_components;
+                    location = {
+                        city: location[0].long_name,
+                        country: location[3].long_name,
+                        country_code: location[3].short_name
+                    }
+                    //localStorage.setItem('location', JSON.stringify(location));
+                    _this.setState({location:location});
+                });
+            }, function(error) {
+                console.log(error);
+                if (error.code === 1) {
+                    //User denied Geolocation
+                    //TODO: show some popup with info
+                }
+            });
+        } else {
+            /* geolocation IS NOT available */
+            console.log('no location available');
+        }
     }
 
     render() {
@@ -56,8 +97,9 @@ class App extends Component {
                 <Sidebar sidebar={<Menu closeSidebar={this.closeSidebar} />}
                     children={(
                         <div>
-                            <Route exact={true} path="/" component={Profile} />
-                            <Route path="/profile" component={Profile} />
+                            <Route exact={true} path="/" render={(props) =>
+                                <Billboard location={this.state.location}/>
+                            } />
                         </div>
                         )}
                     open={this.state.sidebarOpen}
@@ -68,7 +110,7 @@ class App extends Component {
                     contentClassName="Content"
                     styles={this.sidebarStyle} />
                 
-                <MyNowPlaying />
+                <MyNowPlaying location={this.state.location}/>
             </div>
         );
     }
